39ce652a378af147c04417fc7d027717eee9a94d
ranchayasambath <ranchayasambath@gmail.com>
2022-03-08 15:59:03 -0600

    completed maven exercise

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -1,5 +1,21 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+
 public class Example {
     public static void main(String[] args) {
-
+        System.out.print("Enter something: ");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        try {
+            Integer.parseInt(input);
+            System.out.println(input + "is a number.");
+        } catch (NumberFormatException e) {
+            System.out.println(input+ " is not a number.");
+        }
+        System.out.println("Flipped case = "+ StringUtils.swapCase(input));
+        System.out.println("Reversed = " + StringUtils.reverse(input));
     }
 }
